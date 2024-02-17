df9e681a16228376058f0651d42b9c05439d5ada
redpeacock78 <scarletpeacock78@gmail.com>
2024-02-17 20:23:50 +0900

    fix: Fix src/libs/watch_process.cr

--- src/libs/watch_process.cr
+++ src/libs/watch_process.cr
@@ -2,18 +2,18 @@ require "colorize"
 
 private def watch_files(args : NamedTuple(ignore: Array(String) | Array(YAML::Any), match: Array(String) | Array(YAML::Any), ext: Array(String) | Array(YAML::Any)))
   file_list : Array(String) = Dir.glob("./**/*").select { |file| File.file?(file) }
-  Dir.glob("./**/**").select { |file| File.file?(file) }.map do |file|
+  Dir.glob("./**/**").select { |file| File.file?(file) }.each do |file|
     args[:ignore].map { |ignore|
       ignore.to_s.gsub(".", "\\.").gsub("/", "\\/").gsub("**", "*").sub(/^\*/, ".*")
-    }.map { |ignore|
+    }.each { |ignore|
       file_list.delete(file) if file.match(/#{ignore}/)
     } if args[:ignore].size > 0
     args[:match].map { |match|
       match.to_s.gsub(".", "\\.").gsub("/", "\\/").gsub("**", "*").sub(/^\*/, ".*")
-    }.map { |match|
+    }.each { |match|
       file_list.delete(file) if !file.match(/#{match}/)
     } if args[:match].size > 0
-    args[:ext].map { |ext|
+    args[:ext].each { |ext|
       file_list.delete(file) unless file.match(/#{ext}$/)
     } if args[:ext].size > 0
   end
@@ -39,44 +39,47 @@ private def ext_list(args : NamedTuple(ext: Array(String) | Array(YAML::Any), fi
   ext_list
 end
 
+private def watch_message(path_list : Array(String), ext_list : Array(String), cmd : String, status : Bool = false)
+  puts "#{"[*]".colorize(:light_blue)} restarting due to changes..." if status
+  puts "#{"[*]".colorize(:light_blue)} watching path(s): #{path_list.join(", ")}"
+  puts "#{"[*]".colorize(:light_blue)} watching extensions: #{ext_list.join(", ")}"
+  puts "#{"[!]".colorize(:light_yellow)} starting `#{cmd}`"
+end
+
 def watch_process(args)
   file_timestamps : Hash(String, String) = {} of String => String
 
   file_list : Array(String) = watch_files({ignore: args[:ignore], match: args[:match], ext: args[:ext]})
-  path_list : Array(String) = path_list(file_list)
-  ext_list : Array(String) = ext_list({ext: args[:ext], file_list: file_list})
+  path_list : Array(String) = path_list(file_list).sort.uniq
+  ext_list : Array(String) = ext_list({ext: args[:ext], file_list: file_list}).sort.uniq
+
+  watch_message(path_list, ext_list, args[:cmd])
 
   process : Process | Nil = nil
-
-  puts "#{"[*]".colorize(:light_blue)} watching path(s): #{path_list.sort.uniq.join(",")}"
-  puts "#{"[*]".colorize(:light_blue)} watching extensions: #{ext_list.sort.uniq.join(",")}"
-  puts "#{"[!]".colorize(:light_yellow)} starting `#{args[:cmd]}`"
-
   process = Process.new(args[:cmd], shell: true, output: STDOUT, error: STDERR) if args[:env]? == nil
   process = Process.new(args[:cmd], env: args[:env]?, shell: true, output: STDOUT, error: STDERR) if args[:env]? != nil
 
   loop do
     begin
-      file_list.map do |file|
+      file_list.each do |file|
         timestamp : String = File.info(file).modification_time.to_unix.to_s
         if file_timestamps[file]? && file_timestamps[file] != timestamp
           file_timestamps[file] = timestamp
 
-          puts "#{"[*]".colorize(:light_blue)} restarting due to changes..."
-          puts "#{"[*]".colorize(:light_blue)} watching path(s): #{path_list.sort.uniq.join(",")}"
-          puts "#{"[*]".colorize(:light_blue)} watching extensions: #{ext_list.sort.uniq.join(",")}"
-          puts "#{"[!]".colorize(:light_yellow)} starting `#{args[:cmd]}`"
+          watch_message(path_list, ext_list, args[:cmd], true)
 
           process.signal(:kill) unless process.terminated? if process.is_a? Process
           process = Process.new(args[:cmd], shell: true, output: STDOUT, error: STDERR) if args[:env]? == nil
           process = Process.new(args[:cmd], env: args[:env]?, shell: true, output: STDOUT, error: STDERR) if args[:env]? != nil
         else
-          file_timestamps[file]? == nil
           file_timestamps[file] = timestamp
         end
       end
     rescue ex
       puts "#{"[E]".colorize(:light_red)} #{ex.message}"
+      exit(1)
+    ensure
+      sleep 0.5
     end
   end
 end
