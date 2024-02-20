f5c24f36c1c857b835656224bbf52df2df00883d
redpeacock78 <scarletpeacock78@gmail.com>
2024-02-20 13:55:11 +0900

    fix: Fix src/libs/read_conf.cr

--- src/libs/read_conf.cr
+++ src/libs/read_conf.cr
@@ -1,5 +1,6 @@
 require "yaml"
 require "colorize"
+require "../utils/*"
 
 def read_conf
   pwd : String = Dir.current
@@ -8,6 +9,7 @@ def read_conf
   conf_file_path = "#{pwd}/#{conf_file_name}.yml" if File.exists?("#{pwd}/#{conf_file_name}.yml")
   conf_file_path = "#{pwd}/#{conf_file_name}.yaml" if File.exists?("#{pwd}/#{conf_file_name}.yaml")
   if conf_file_path.size == 0
+    puts "#{"[*]".colorize(:light_blue)} v#{version}"
     puts "#{"[E]".colorize(:light_red)} Cannot find `#{conf_file_name}.yml` or `#{conf_file_name}.yaml` in the current directory!"
     puts "#{"[*]".colorize(:light_blue)} You can create a config to add scripts to with `#{File.basename(Process.executable_path.to_s).colorize(:light_blue)} #{"--init".colorize(:light_yellow)}`."
     exit(1)
@@ -15,6 +17,7 @@ def read_conf
   begin
     YAML.parse(File.read(conf_file_path))
   rescue ex
+    puts "#{"[*]".colorize(:light_blue)} v#{version}"
     puts "#{"[E]".colorize(:light_red)} Failed to read configuration file - #{ex.message}"
     exit(1)
   end
